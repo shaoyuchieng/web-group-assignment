249707868f63c63b8da11e72f11f840825d98200
shaoyuchieng <shaoyuchieng@gmail.com>
2025-05-02 18:19:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ function showCategory(category) {
     });
   }
 
-// 初始化Lightbox时设置居中选项
-lightbox.option({
-  'resizeDuration': 200,
-  'wrapAround': true,
-  'positionFromTop': 100, // 你可以调整这个值来微调垂直位置
-  'disableScrolling': true
-});
+
   showCategory('interior'); // 默认显示室内
   
